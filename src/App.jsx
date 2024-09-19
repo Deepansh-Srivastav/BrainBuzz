1a856e944c55c4c05813e8dfaaf149a359d2a4c8
Deepansh Srivastav <deepanshsrivastav9711@gmail.com>
2024-09-19 22:16:44 +0530

    Quiz Component added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
 
     return (
         <>
-            <Header/>
-            <Quiz/>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
         </>
     )
 }
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import { useState } from "react"
+import QUESTIONS from "../Data/questions"
 
 const Quiz = () => {
-  return (
-    <p>
-        Currently Active question
-        
-    </p>
-  )
+
+    const [userAnswers, setUserAnswers] = useState([])
+
+    const activeQuestionIndex = userAnswers.length
+
+    function handleSelectedAnswer(newAnswer) {
+
+        setUserAnswers((currentListOfAnswers) => {
+
+            return [...currentListOfAnswers, newAnswer]
+
+        })
+    }
+
+    return (
+        <div id="quiz">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                    return (<li key={answer} className="answer">
+                        <button onClick={() => {
+                            handleSelectedAnswer(answer)
+                        }}>{answer}</button>
+                    </li>)
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Quiz
